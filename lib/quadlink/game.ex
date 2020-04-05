b6f2aba936ef361c25d1a4d867d07a24a5cc3a57
Nolan Tiffney <nolantiffney@gmail.com>
2020-04-04 21:30:02 -0400

    Add presence and enhancements.

--- lib/quadlink/game.ex
+++ lib/quadlink/game.ex
@@ -4,45 +4,90 @@ defmodule Quadlink.Game do
   @board %{1 => %{}, 2 => %{}, 3 => %{}, 4 => %{}, 5 => %{}, 6 => %{}, 7 => %{}}
 
   defmodule State do
-    defstruct [:id, :board, :yellow, :purple]
+    defstruct [:id, :board, :turn, :game_state]
   end
 
   def start_link(id) do
     GenServer.start_link(__MODULE__, id, name: via_tuple(id))
   end
 
-  def make_move(id, color, column) do
-    GenServer.cast(via_tuple(id), {:make_move, color, column})
+  def make_move(id, name, column) do
+    GenServer.cast(via_tuple(id), {:make_move, name, column})
   end
 
-  def register_player(id) do
-    GenServer.call(via_tuple(id), :register)
+  def register_player(id, name) do
+    GenServer.call(via_tuple(id), {:register, name})
+  end
+
+  def reset(id) do
+    GenServer.cast(via_tuple(id), :reset)
+  end
+
+  def get_game_state(id) do
+    GenServer.call(via_tuple(id), :get_game_state)
   end
 
   @impl true
   def init(id) do
     :timer.send_interval(30_000, self(), :check_disconnect)
 
-    {:ok, %State{board: @board, id: id}}
+    {:ok, %State{board: @board, id: id, turn: "yellow", game_state: :playing}}
   end
 
   @impl true
-  def handle_call(:register, {pid, _}, state) do
-    case {state.yellow, state.purple} do
-      {nil, _} ->
-        {:reply, {:ok, "yellow"}, Map.put(state, :yellow, pid)}
+  def handle_call({:register, name}, {pid, _}, state) do
+    topic = "players:" <> state.id
+    players = QuadlinkWeb.Presence.list(topic)
 
-      {_, nil} ->
-        {:reply, {:ok, "purple"}, Map.put(state, :purple, pid)}
-
-      _ ->
+    cond do
+      Enum.count(players) == 2 ->
         {:reply, {:error, :full}, state}
+
+      name in Map.keys(players) ->
+        {:reply, {:error, :name_taken}, state}
+
+      true ->
+        available_colors =
+          players
+          |> Enum.map(fn {_name, atts} -> hd(atts.metas).color end)
+          |> (fn taken -> ["yellow", "purple"] -- taken end).()
+
+        selected_color = hd(available_colors)
+
+        ### register the player's pid for tracking
+        QuadlinkWeb.Presence.track(
+          pid,
+          topic,
+          name,
+          %{online_at: inspect(System.system_time(:second)), color: selected_color}
+        )
+
+        {:reply, {:ok, selected_color, state.game_state}, state}
     end
   end
 
   @impl true
-  def handle_cast({:make_move, color, column}, state) do
-    if Enum.count(state.board[column]) < 6 do
+  def handle_call(:get_game_state, _, state) do
+    {:reply, state.game_state, state}
+  end
+
+  @impl true
+  def handle_cast({:make_move, name, column}, state) do
+    ### Get the user's color, verifying they are still playing
+    ### by checking the presence.
+    color =
+      QuadlinkWeb.Presence.list("players:" <> state.id)
+      |> Map.get(name)
+      |> Map.get(:metas)
+      |> hd()
+      |> Map.get(:color)
+
+    condition =
+      state.game_state == :playing &&
+        color == state.turn &&
+        Enum.count(state.board[column]) < 6
+
+    if condition do
       ### update board
       row =
         state.board[column]
@@ -52,22 +97,55 @@ defmodule Quadlink.Game do
 
       board = Map.update!(state.board, column, &Map.put(&1, row, color))
 
-      ###
-
       ### check win
       ###
       win = check_win(board, column, row)
 
       Phoenix.PubSub.broadcast(Quadlink.PubSub, "game:" <> state.id, {:update, board})
 
-      if win do
-        Phoenix.PubSub.broadcast(Quadlink.PubSub, "game:" <> state.id, {:winner, color})
-      end
+      color_prime = hd(["yellow", "purple"] -- [color])
 
-      {:noreply, Map.put(state, :board, board)}
+      state_prime =
+        state
+        |> Map.put(:board, board)
+        |> Map.put(:turn, color_prime)
+
+      if win do
+        Phoenix.PubSub.broadcast(Quadlink.PubSub, "game:" <> state.id, {:winner, name})
+        {:noreply, Map.put(state_prime, :game_state, :game_over)}
+      else
+        {:noreply, state_prime}
+      end
     else
       {:noreply, state}
     end
+  end
+
+  @impl true
+  def handle_cast(:reset, state) do
+    Phoenix.PubSub.broadcast(Quadlink.PubSub, "game:" <> state.id, :reset)
+
+    state
+    |> Map.put(:board, @board)
+    |> Map.put(:turn, "yellow")
+    |> Map.put(:game_state, :playing)
+    |> (fn st -> {:noreply, st} end).()
+  end
+
+  @impl true
+  def handle_info(:check_disconnect, state) do
+    players = QuadlinkWeb.Presence.list("players:" <> state.id)
+
+    if Enum.count(players) == 0 do
+      {:stop, :normal, state}
+    else
+      {:noreply, state}
+    end
+  end
+
+  @impl true
+  def terminate(_reason, _state) do
+    :normal
   end
 
   defp check_win(board, col, row) do
@@ -112,23 +190,6 @@ defmodule Quadlink.Game do
     |> Enum.any?(fn set ->
       MapSet.new(set) |> Enum.count() == 1
     end)
-  end
-
-  @impl true
-  def handle_info(:check_disconnect, state) do
-    yellow = (state.yellow && Process.alive?(state.yellow)) || false
-    purple = (state.purple && Process.alive?(state.purple)) || false
-
-    if not (yellow or purple) do
-      {:stop, :normal, state}
-    else
-      {:noreply, state}
-    end
-  end
-
-  @impl true
-  def terminate(_reason, _state) do
-    :normal
   end
 
   defp via_tuple(id) do
--- lib/quadlink_web/live/game_live/play.ex
+++ lib/quadlink_web/live/game_live/play.ex
@@ -3,51 +3,117 @@ defmodule QuadlinkWeb.GameLive.Play do
 
   alias Quadlink.GameSupervisor
   alias Quadlink.Game
+  alias QuadlinkWeb.Router.Helpers, as: Routes
 
   import Phoenix.HTML.Form
 
   def render(assigns) do
     ~L"""
-    <%= if not(@joined) do %>
-      <%= f = form_for :login, "#", [phx_submit: :register] %>
-        <%= text_input f, :name %>
-        <%= submit "Enter" %>
-      </form>
-    <% else %>
-      <table>
-        <tr>
+    <p class="alert alert-info"><%= live_flash(@flash, :info) %></p>
+    <p class="alert alert-danger"><%= live_flash(@flash, :error) %></p>
+    <%= case @state do %>
+      <% :waiting -> %>
+        <%= f = form_for :login, "#", [phx_submit: :register] %>
+          <%= text_input f, :name %>
+          <%= submit "Enter" %>
+        </form>
+        <%= live_redirect "Spectate Game", to: Routes.live_path(@socket, QuadlinkWeb.GameLive.Spectate, @server) %>
+
+      <% :playing -> %>
+        <div class="ctext"> Choose a column: </div>
+        <section class="controller">
           <%= for col <- 1..7 do %>
-            <td><button phx-click="<%= col %>"><%= col %></button></td>
+            <div class="cinput" >
+              <button class="cbutton-<%= @color %>" phx-click="<%= col %>">
+                <%= col %>
+              </button>
+            </div>
           <% end %>
-        </tr>
-      </table>
+        </section>
+
+      <% :game_over -> %>
+        <button phx-click="restart">New Game</button>
     <% end %>
     """
   end
 
   def mount(%{"id" => id}, _session, socket) do
-    GameSupervisor.start_child(id)
-
     socket
     |> assign(:server, id)
-    |> assign(:joined, false)
+    |> assign(:state, :waiting)
     |> to_tuple(:ok)
   end
 
-  def handle_event("register", _, socket) do
-    {:ok, color} = Game.register_player(socket.assigns[:server])
+  def handle_event(
+        "register",
+        %{"login" => %{"name" => name}},
+        %{assigns: %{server: server}} = socket
+      ) do
+    GameSupervisor.start_child(server)
 
-    socket
-    |> assign(:color, color)
-    |> assign(:joined, true)
-    |> to_tuple(:noreply)
+    case Game.register_player(server, name) do
+      {:error, :full} ->
+        socket
+        |> put_flash(:error, "The game is full")
+        |> to_tuple(:noreply)
+
+      {:error, :name_taken} ->
+        socket
+        |> put_flash(:error, "That name has been taken")
+        |> to_tuple(:noreply)
+
+      {:ok, color, game_state} ->
+        QuadlinkWeb.Endpoint.subscribe("game:" <> server)
+
+        socket
+        |> clear_flash()
+        |> assign(:color, color)
+        |> assign(:name, name)
+        |> assign(:state, game_state)
+        |> to_tuple(:noreply)
+    end
+  end
+
+  def handle_event("restart", _params, socket) do
+    Game.reset(socket.assigns[:server])
+
+    restart(socket)
   end
 
   def handle_event(pos, _params, socket) do
-    Game.make_move(socket.assigns[:server], socket.assigns[:color], String.to_integer(pos))
+    Game.make_move(socket.assigns[:server], socket.assigns[:name], String.to_integer(pos))
 
     {:noreply, socket}
   end
 
+  def handle_info({:winner, name}, socket) do
+    message =
+      if name == socket.assigns[:name] do
+        "You Win!"
+      else
+        name <> " Wins!"
+      end
+
+    socket
+    |> assign(:state, :game_over)
+    |> put_flash(:info, message)
+    |> to_tuple(:noreply)
+  end
+
+  def handle_info(:reset, socket) do
+    restart(socket)
+  end
+
+  def handle_info(_, socket) do
+    {:noreply, socket}
+  end
+
   defp to_tuple(socket, atom) when is_atom(atom), do: {atom, socket}
+
+  defp restart(socket) do
+    socket
+    |> assign(:state, :playing)
+    |> clear_flash()
+    |> to_tuple(:noreply)
+  end
 end
